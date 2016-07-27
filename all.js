291f11e05ffa5d90bfeecabbf1df3b853638faaf
Dan Denney <dd.subscriptions@gmail.com>
2016-07-26 20:35:11 -0400

    Add source for selecting elements by attribute

--- all.js
+++ all.js
@@ -1,4 +1,5 @@
 // Select elements by attribute
+// Source: http://codereview.stackexchange.com/questions/14061/javascript-function-to-get-dom-elements-by-any-attribute
 
 function getElements(attrib) {
     return document.querySelectorAll('[' + attrib + ']');
