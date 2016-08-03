6b01a181e3204c6b6c7407d7851a02514fdd2f53
Dan Denney <dd.subscriptions@gmail.com>
2016-08-03 08:03:39 -0400

    Add toggleClass

--- all.js
+++ all.js
@@ -2,7 +2,28 @@
 // Source: http://codereview.stackexchange.com/questions/14061/javascript-function-to-get-dom-elements-by-any-attribute
 
 function getElements(attrib) {
-    return document.querySelectorAll('[' + attrib + ']');
+  return document.querySelectorAll('[' + attrib + ']');
 }
 
-var elements = getElements('required');
+var elementsByAttribute = getElements('required');
+
+// Toggle Class
+// Source: http://youmightnotneedjquery.com
+
+function toggleClass() {
+
+  if (el.classList) {
+    el.classList.toggle(className);
+  } else {
+    var classes = el.className.split(' ');
+    var existingIndex = classes.indexOf(className);
+
+    if (existingIndex >= 0)
+      classes.splice(existingIndex, 1);
+    else
+      classes.push(className);
+
+    el.className = classes.join(' ');
+  }
+
+};
