adb2ba75c52f112d8aacc05e163c780d3b3c48d9
Dan Denney <dd.subscriptions@gmail.com>
2016-08-25 18:28:20 -0400

    Add a way to check if a value is in an array

--- all.js
+++ all.js
@@ -1,6 +1,12 @@
 // # `all.js`
 // This file is a collection of snippets that I want as a reference without having to google them again
 
+// #### Check if a value is in an array
+// Source: http://stackoverflow.com/questions/7378228/check-if-an-element-is-present-in-an-array?answertab=votes#tab-top
+function isInArray(value, array) {
+  return array.indexOf(value) > -1;
+}
+console.log(isInArray(1, [1,2,3])); // true
 
 // #### Select elements by attribute
 // Source: http://codereview.stackexchange.com/questions/14061/javascript-function-to-get-dom-elements-by-any-attribute
